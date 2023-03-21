2db02bee693dc4a4ce39834f61e8996eaad05876
Lev Seregin <sereginlev1437@gmail.com>
2023-03-21 16:10:18 +0100

    added css module for 'Main' file in 'Confirm' folder. Deleted 'Main.scss' file from 'Confirm' folder. Added 'Form.module.scss'to 'modules/Authentification' folder

--- src/components/Confirm/Main/Main.jsx
+++ src/components/Confirm/Main/Main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-import './Main.scss';
+import styles from 'scss/modules/Authentification/Main.module.scss';
 
 function Main() {
 	const { t } = useTranslation();
 
 	return (
-		<div className='main-auth'>
-			<p className='main-auth__text'>{ t("authConfirmEmail") }</p>
-			<Link className='main-auth__btn' to='/auth'>{ t("authContinue") }</Link>
+		<div className={styles.root}>
+			<p className={styles.text}>{ t("authConfirmEmail") }</p>
+			<Link className={styles.btn} to='/auth'>{ t("authContinue") }</Link>
 		</div>
 	)
 }
