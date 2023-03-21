3e116be834e367285fa6de4e5e65a9b62aac900d
Lev Seregin <sereginlev1437@gmail.com>
2023-03-21 11:53:43 +0100

    fixes imports of translation files in i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,8 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from 'react-i18next';
 
-import translationEN from "src/locales/en/translation.json";
-import translationRU from "src/locales/ru/translation.json";
+import translationEN from "./src/locales/en/translation.json";
+import translationRU from "./src/locales/ru/translation.json";
 
 const resources = {
   en: {
