bdf7d01c760a6f47416002f036d0e26e421ea768
Lev Seregin <sereginlev1437@gmail.com>
2023-03-21 11:48:17 +0100

    replace 'locales' folder to src. added some fixes to 'i18n.js' file

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,21 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from 'react-i18next';
 
+import translationEN from "src/locales/en/translation.json";
+import translationRU from "src/locales/ru/translation.json";
+
+const resources = {
+  en: {
+	 translation: translationEN
+  },
+  ru: {
+	 translation: translationRU
+  }
+};
+
 i18next.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-	fallbackLng: 'en',
+	resources,
+	lng: 'en',
 	debug: true,
 	detection: {
 		order: ['queryString', 'cookie'],
